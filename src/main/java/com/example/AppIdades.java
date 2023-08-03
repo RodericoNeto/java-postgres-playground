38e0023a7b31aa075b0cbbfa441330c5a0048c29
Roderico Neto <rodericocrneto@gmail.com>
2023-08-03 04:18:05 +0000

    idades e datas

---
+++ src/main/java/com/example/AppIdades.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class AppIdades {
+    public static void main(String[] args) {
+        System.out.println("uobaaa");
+
+        var cidadao01 = new Cidadao();
+        cidadao01.setDataNascimento(LocalDate.of(1992, 04, 1));
+        System.out.println(cidadao01.idade());
+    }
+}
---
+++ src/main/java/com/example/Cidadao.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class Cidadao {
+    private LocalDate dataNascimento;
+
+
+    public LocalDate getDataNascimento(){
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int idade(){
+        return LocalDate.now().getYear() - dataNascimento.getYear();
+    }
+
+    
+}

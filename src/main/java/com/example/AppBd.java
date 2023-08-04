95f9fd9b3b2176858ef74cdc3d45c6912f5fed12
Roderico Neto <rodericocrneto@gmail.com>
2023-08-04 03:14:42 +0000

    crud

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -23,9 +23,104 @@ public class AppBd {
             listarEstados(conn);
 
             localizarEstado(conn, "CE");
+
+            
+
+            var marca = new Marca();
+            marca.setId(1L);
+
+            // # PARA INSERIR PRODUTO #
+            // var produto = new Produto();
+            // produto.setMarca(marca);
+            // produto.setNome("Produto teste");
+            // produto.setValor(100);
+
+            // # PARA ALTERAR PRODUTO #
+            var produto = new Produto();
+            produto.setId(205L);
+            produto.setMarca(marca);
+            produto.setNome("Produto Novo (alterado)");
+            produto.setValor(90);
+
+            //inserirProduto(conn, produto);
+            alterarProduto(conn, produto);
+            excluirProduto(conn, 203L);
+
+            listarDadosTabela(conn, "produto");
         } catch (SQLException e) {
             System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
         }
+    }
+
+    private void excluirProduto(Connection conn, long id) {
+        var sql = "delete from produto where id = ?";
+        
+        try {
+            var statement = conn.prepareStatement(sql);
+            statement.setLong(1, id);
+            if(statement.executeUpdate() == 1){
+                System.out.printf("\nProduto %d excluido \n", id);
+            } else System.out.println("Produto não localizado");
+        } catch (SQLException e) {
+            System.err.println("Erro ao excluir: " + e.getMessage());
+        }
+        
+    }
+
+    private void inserirProduto(Connection conn, Produto produto){
+        var sql = "insert into produto (nome, marca_id, valor) values (? , ?, ?)";
+        try {
+            var statement = conn.prepareStatement(sql);
+            statement.setString(1, produto.getNome());
+            statement.setLong(2, produto.getMarca().getId());
+            statement.setDouble(3, produto.getValor());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro na execução da consulta: " + e.getMessage());
+        };
+    }
+
+
+    private void alterarProduto(Connection conn, Produto produto){
+        var sql = "update produto set nome = ?, marca_id = ?, valor = ? where id = ?";
+        try {
+            var statement = conn.prepareStatement(sql);
+            statement.setString(1, produto.getNome());
+            statement.setLong(2, produto.getMarca().getId());
+            statement.setDouble(3, produto.getValor());
+            statement.setLong(4, produto.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro na alteração do produto: " + e.getMessage());
+        };
+    }
+
+    private void listarDadosTabela(Connection conn, String tabela) {
+        var sql = "select * from " + tabela;
+        //System.out.println(sql);
+        try{
+            var statement = conn.createStatement();
+            var result = statement.executeQuery(sql);
+    
+            var metadata = result.getMetaData();
+            int cols = metadata.getColumnCount();
+
+            for(int i = 1; i <= cols; i++){
+                System.out.printf("%-10s | ", metadata.getColumnName(i));
+            }
+            System.out.println();
+
+            while(result.next()){
+
+                for(int i = 1; i <= cols; i++){
+                    System.out.printf("%-10s | ", result.getString(i));
+                }
+                System.out.println();
+            }
+        } catch(SQLException e){
+            System.err.println("Erro na execução da consulta: " + e.getMessage());
+        }
+        
     }
 
     private void localizarEstado(Connection conn, String uf) {
